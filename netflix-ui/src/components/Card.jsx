1f8e05deb9b7eefc547a391544fce8ef50c75b9e
RaveenaBhasin <raveenabhasin15@gmail.com>
2022-12-25 10:11:45 +0530

    replace localhost with deployed server link

--- netflix-ui/src/components/Card.jsx
+++ netflix-ui/src/components/Card.jsx
@@ -27,7 +27,7 @@ export default React.memo(function Card({ index, movieData, isLiked = false }) {
 
   const addToList = async () => {
     try {
-      await axios.post("http://localhost:5000/api/user/add", {
+      await axios.post("https://netflix-clone-mern.vercel.app/api/user/add", {
         email,
         data: movieData,
       });
--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -81,7 +81,7 @@ export const getUsersLikedMovies = createAsyncThunk(
   async (email) => {
     const {
       data: { movies },
-    } = await axios.get(`http://localhost:5000/api/user/liked/${email}`);
+    } = await axios.get(`https://netflix-clone-mern.vercel.app/api/user/liked/${email}`);
     return movies;
   }
 );
@@ -91,7 +91,7 @@ export const removeMovieFromLiked = createAsyncThunk(
   async ({ movieId, email }) => {
     const {
       data: { movies },
-    } = await axios.put("http://localhost:5000/api/user/remove", {
+    } = await axios.put("https://netflix-clone-mern.vercel.app/api/user/remove", {
       email,
       movieId,
     });
